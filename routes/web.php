408c01d9fafdd4043001822fe196fc434fbea78b
asaber3030 <abdulrahmansaber70@gmail.com>
2025-05-17 00:35:14 +0300

    add

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 
+Route::get("/", function () {
+  return response()->json([
+    'message' => 'Welcome to the API',
+    'status' => 200
+  ]);
+});
+
 Route::get('home', [HomeController::class, 'home'])->name('home');
 Route::post('home', [HomeController::class, 'upload'])->name('home.upload');
